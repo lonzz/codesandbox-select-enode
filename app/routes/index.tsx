b0bc88d48e42917b91f1d3b3dd10bf20abf66478
Lonzz <lonzz@lonzz.com>
2023-02-24 09:07:35 +0000

    Dummy animation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,19 @@
 import { VersionSelect } from "~/components/VersionSelect";
-import { SidebarLayout } from "~/components/Layout";
 import { layout, sprinkles } from "~/styles";
 
 export default function Index() {
   return (
-      <div style={{
+    <div
+      style={{
         width: 256,
-        padding: '16px 12px 16px 12px'
-      }}>
-        <VersionSelect
-          className={sprinkles({ mb: 16 })}
-          versions={["2023-03-01","2023-02-01"]}
-          hash={""} />
-        </div>
+        padding: "16px 12px 16px 12px",
+      }}
+    >
+      <VersionSelect
+        className={sprinkles({ mb: 16 })}
+        versions={["2023-03-01", "2023-02-01"]}
+        hash={""}
+      />
+    </div>
   );
 }
